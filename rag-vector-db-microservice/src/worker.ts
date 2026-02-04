e0281f0315ee5ee67189544899878fa27e5cb320
Zohaib Irshad <zohaibirshad678@gmail.com>
2026-02-04 19:22:13 +0500

    update

--- rag-vector-db-microservice/src/worker.ts
+++ rag-vector-db-microservice/src/worker.ts
@@ -1,12 +1,21 @@
+import { config } from "dotenv";
+config();
+
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 import { Worker } from "bullmq";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { QdrantVectorStore } from "@langchain/qdrant";
+
+console.log("OPenAI Key:", !!process.env.OPENAI_API_KEY);
+console.log("Qdrant URL:", !!process.env.QDRANT_URL);
 
 const worker = new Worker('file-processing', async job => {
     const data = JSON.parse(job.data);
 
     const loader = new PDFLoader(data.filePath);
     const docs = await loader.load();
+    console.log(`PDF ${data.originalName} loaded with ${docs[0]} pages`);
 
     const textSplitter = new RecursiveCharacterTextSplitter({
         chunkSize: 1000,
@@ -14,10 +23,24 @@ const worker = new Worker('file-processing', async job => {
     });
 
     const chunks = await textSplitter.splitDocuments(docs);
-    
-    console.log(`Split into ${chunks.length} chunks`);
-    console.log(`Loaded ${docs.length} documents from ${docs}`);
+    console.log(`PDF ${data.originalName} split into ${chunks.length} chunks`);
 
+    const embeddings = new OpenAIEmbeddings({
+        model: "text-embedding-3-large",
+        apiKey: process.env.OPENAI_API_KEY as string,
+    });
+
+    console.log("Creating or connecting to Qdrant collection...");
+
+    const vectorStore = await QdrantVectorStore.fromExistingCollection(embeddings, {
+        url: process.env.QDRANT_URL as string,
+        collectionName: "langchainjs-testing",
+    });
+
+    console.log(`Adding ${chunks.length} chunks to Qdrant collection...`);
+
+    await vectorStore.addDocuments(chunks);
+    console.log(`Added ${chunks.length} chunks to Qdrant collection.`);
 
 }, {
     concurrency: 100, connection: {
